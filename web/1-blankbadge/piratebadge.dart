c0f879737f645c43ed5784f1c75e39c2a10601b7
Bradley Wogsland <bwogsland@leadspedia.com>
2014-08-08 09:27:17 -0500

    steps 1-3

--- web/1-blankbadge/piratebadge.dart
+++ web/1-blankbadge/piratebadge.dart
@@ -1,7 +1,38 @@
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+import 'dart:html';
 
+//vars
+ButtonElement genButton;
+
+//funkytowns
 void main() {
-  // Your app starts here.
+  // input box
+  querySelector('#inputName').onInput.listen(updateBadge);
+  
+  // button
+  genButton = querySelector('#generateButton');
+  genButton.onClick.listen(generateBadge);
+}
+
+void updateBadge(Event e) {
+  //querySelector('#badgeName').text = (e.target as InputElement).value;
+  String inputName = (e.target as InputElement).value;
+  setBadgeName(inputName);
+  if (inputName.trim().isEmpty) {
+    genButton..disabled = false
+             ..text = "Now, tell us it's nameses!";
+  } else {
+    genButton..disabled = true
+             ..text = "Stupid, fat hobbit.";
+  }
+}
+
+void setBadgeName(String newName) {
+  querySelector('#badgeName').text = newName;
+}
+
+void generateBadge(Event e) {
+  setBadgeName('Inigo Montoya. You killed my father; prepare to die.');
 }
