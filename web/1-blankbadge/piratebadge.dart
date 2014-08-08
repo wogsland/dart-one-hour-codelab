5af4c4ebc989c8235682347508cb290d7030089a
Bradley Wogsland <bradley@wogsland.org>
2014-08-08 10:30:56 -0500

    step 6

--- web/1-blankbadge/piratebadge.dart
+++ web/1-blankbadge/piratebadge.dart
@@ -6,9 +6,11 @@
 // supposedly "code bloat is not an issue because of tree-shaking"
 import 'dart:html';
 import 'dart:math' show Random; //but here we're only grabbing one class
+import 'dart:convert' show JSON; //sure, the syntax is all Java, but we still like JSONs
 
-//vars
+//vars & constants
 ButtonElement genButton;
+final String TREASURE_KEY = 'pirateName';
 
 //funkytowns
 void main() {
@@ -18,6 +20,7 @@ void main() {
   // button
   genButton = querySelector('#generateButton');
   genButton.onClick.listen(generateBadge);
+  setBadgeName(getBadgeNameFromStorage());
 }
 
 void updateBadge(Event e) {
@@ -34,7 +37,20 @@ void updateBadge(Event e) {
 }
 
 void setBadgeName(PirateName newName) {
+  if (newName == null) {
+    return;
+  }
   querySelector('#badgeName').text = newName.pirateName;
+  window.localStorage[TREASURE_KEY] = newName.jsonString;
+}
+
+PirateName getBadgeNameFromStorage() {
+  String storedName = window.localStorage[TREASURE_KEY];
+  if (storedName != null) {
+    return new PirateName.fromJSON(storedName);
+  } else {
+    return null;
+  }
 }
 
 void generateBadge(Event e) {
@@ -52,7 +68,7 @@ class PirateName {
   static final List names = ['Jor','Lara','Kal','John','Martha','Clark'];
   static final List appellations = ['El','Kent'];
   
-  // constructor
+  // constructors
   PirateName({String firstName, String appellation}) {
     if (firstName == null) {
       _firstName = names[indexGen.nextInt(names.length)];
@@ -65,10 +81,17 @@ class PirateName {
       _appellation = appellation;
     }
   }
+  PirateName.fromJSON(String jsonString) {
+    Map storedName = JSON.decode(jsonString);
+    _firstName = storedName['f'];
+    _appellation = storedName['a'];
+  }
   
-  String toString() => pirateName; //mission critical line left out of tutorial
+  
   
   // other methods
+  String toString() => pirateName; //mission critical line left out of tutorial
   String get pirateName => _firstName.isEmpty ? '' : '$_firstName the $_appellation';
+  String get jsonString => JSON.encode({"f": _firstName, "a": _appellation});
 }
 
